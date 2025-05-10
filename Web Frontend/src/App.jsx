9a431cba2f6ba0a9bb6d3d9ec01bea2ff6bd1a89
Mayank Sachin chougale <136550347+Mayank202004@users.noreply.github.com>
2025-05-10 10:09:22 +0530

    Added toggle theme logic
    Closes #6

--- Web Frontend/src/App.jsx
+++ Web Frontend/src/App.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useEffect } from 'react'
 import Topbar from './components/Topbar/Topbar'
 
 const App = () => {
+
+  const [theme,setTheme] = useState('dark')
+
+  /**
+   * @desc: THis hook is ued to set the theme to add (toggles class='' and class='dark' to html tag based on theme value above)
+   */
+  useEffect(() => {
+    document.documentElement.classList.toggle('dark', theme === 'dark');
+  }, [theme]);
+  
   return (
-    <main className="relative min-h-screen w-screen overflow-x-hidden">
-      <Topbar/>
-    </main>
+    <main className="relative min-h-screen w-screen overflow-x-hidden transition-colors duration-500 bg-white text-black dark:bg-gray-800 dark:text-white">
+      <Topbar theme={theme} setTheme={setTheme} />
+</main>
+
   )
 }
 
