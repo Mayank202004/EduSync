705927120d9e4dbc41eb137fc21e1bd921cd3112
Mayank Sachin chougale <136550347+Mayank202004@users.noreply.github.com>
2025-05-11 10:07:18 +0530

    Work on login page
    Work on #9
    Added rive animation

---
+++ Web Frontend/src/components/Login/isoCubeAnimation.jsx
@@ -0,0 +1,37 @@
+import { useRive, useStateMachineInput } from "@rive-app/react-canvas";
+import { useEffect } from "react";
+import animationFile from "../../assets/isoCube.riv";
+
+const IsoCubeAnimation = () => {
+  const STATE_MACHINE_NAME = "State Machine 1";
+  const HOVER_INPUT_NAME = "Hovered"; 
+
+  const { RiveComponent, rive } = useRive({
+    src: animationFile,
+    artboard: "Artboard",
+    stateMachines: [STATE_MACHINE_NAME],
+    autoplay: true,
+  });
+
+  const hoverInput = useStateMachineInput(rive, STATE_MACHINE_NAME, HOVER_INPUT_NAME);
+
+  const handleMouseEnter = () => {
+    if (hoverInput) hoverInput.value = true;
+  };
+
+  const handleMouseLeave = () => {
+    if (hoverInput) hoverInput.value = false;
+  };
+
+  return (
+    <div
+      className="w-lvh h-full"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <RiveComponent />
+    </div>
+  );
+};
+
+export default IsoCubeAnimation;
