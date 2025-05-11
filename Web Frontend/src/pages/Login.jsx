f7e5eaf8db62a962a4557da366400f582fe84e80
Mayank Sachin chougale <136550347+Mayank202004@users.noreply.github.com>
2025-05-11 10:05:26 +0530

    Work on login page
    Added rive animation

---
+++ Web Frontend/src/pages/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import IsoCubeAnimation from '../components/Login/isoCubeAnimation'
+
+function Login() {
+  return (
+    <>
+        <div className='w-full h-screen flex items-start'>
+          <IsoCubeAnimation/>
+        </div>
+    </>
+    
+  )
+}
+
+export default Login
