6c531f9e2404958dc68c0e440d20ad6538bd120f
Mayank Sachin chougale <136550347+Mayank202004@users.noreply.github.com>
2025-05-13 09:14:48 +0530

    Added resources page
    Closes #12
    Added resources page

---
+++ Web Frontend/src/pages/Resources.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ResourcesCard from '@/components/Resources/ResourcesCard'
+
+function Resources() {
+  return (
+    <div className='w-full h-screen flex items-center justify-center'>
+        <ResourcesCard/>
+    </div>
+  )
+}
+
+export default Resources
