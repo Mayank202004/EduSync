dd6f236ff8dcebcd1c7db75f14de3a65c1293c77
Mayank Sachin chougale <136550347+Mayank202004@users.noreply.github.com>
2025-05-14 14:10:05 +0530

    Create Routes.jsx
    Created routes and made it more readable

---
+++ Web Frontend/src/routes/Routes.jsx
@@ -0,0 +1,25 @@
+import { Route, createRoutesFromElements } from 'react-router-dom';
+import ProtectedRoute from '@/components/ProtectedRoute';
+import Layout from '@/layout';
+import Login from '@/pages/Login';
+import Home from '@/components/Home/Home';
+import Fees from '@/components/Fees/Fees';
+import Calendar from '@/components/Calendar/Calendar';
+import Resources from '@/pages/Resources';
+import App from '@/App';
+
+const routes = createRoutesFromElements(
+  <>
+    <Route path="/login" element={<Login />} />
+
+    {/* Protect all routes under Layout at once */}
+    <Route element={<ProtectedRoute><App /></ProtectedRoute>}>
+      <Route path="/" element={<Layout/>} />
+      <Route path="/fees" element={<Fees />} />
+      <Route path="/calendar" element={<Calendar />} />
+      <Route path="/resources" element={<Resources />} />
+    </Route>
+  </>
+);
+
+export default routes;
