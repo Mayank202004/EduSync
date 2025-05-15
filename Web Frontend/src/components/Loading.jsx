eb1f74496f9f7b910222681447f3a8f30de7836a
Mayank Sachin chougale <136550347+Mayank202004@users.noreply.github.com>
2025-05-15 13:00:58 +0530

    Create Loading.jsx
    Added loading screen ui

---
+++ Web Frontend/src/components/Loading.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const LoadingScreen = () => {
+  return (
+    <div className="h-screen w-screen flex flex-col items-center justify-center dark:bg-black text-gray-800 bg-white">
+      <div className="relative w-16 h-16">
+        <div className="absolute inset-0 rounded-full border-4 dark:border-gray-300 border-gray-900"></div>
+        <div className="absolute inset-0 rounded-full border-4 border-t-blue-600 animate-spin"></div>
+      </div>
+      <p className="mt-4 text-lg font-medium text-black dark:text-white">Loading...</p>
+    </div>
+  );
+};
+
+export default LoadingScreen;
