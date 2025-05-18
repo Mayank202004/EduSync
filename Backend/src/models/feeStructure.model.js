249909cc016c4eb309e55c64bc0b3b73c129871b
Mayank Sachin chougale <136550347+Mayank202004@users.noreply.github.com>
2025-05-18 13:26:04 +0530

    Separated fee structure item
    Separated fee structure item for easy population of data based on item _id

--- Backend/src/models/feeStructure.model.js
+++ Backend/src/models/feeStructure.model.js
@@ -11,16 +11,18 @@ const feeStructureSchema = new Schema({
       enum: ['Tuition Fee', 'Transport Fee', 'Other Fee'],
       required: true
     },
-    structure: [
-      {
-        title: { type: String, required: true },
-        dueDate: Date,
-        compulsory: { type: Boolean, default: true },
-        amount: { type: Number, required: true },
-        discount: { type: Number, default: 0 }
-      }
-    ]
+    structure: [{ type: Schema.Types.ObjectId, ref: 'FeeItem' }]
 }]
 });
 
+
+const feeItemSchema = new Schema({
+  title: { type: String, required: true },
+  dueDate: Date,
+  compulsory: { type: Boolean, default: true },
+  amount: { type: Number, required: true },
+  discount: { type: Number, default: 0 }
+});
+
+export const FeeItem = mongoose.model('FeeItem', feeItemSchema);
 export const FeeStructure = mongoose.model('FeeStructure', feeStructureSchema);
