b888c040bda281218c8dd6db8aa9e4edacb7e0df
Mayank Sachin chougale <136550347+Mayank202004@users.noreply.github.com>
2025-05-18 12:57:55 +0530

    Added static fee structure schema
    Work on #32
    Added static fee structure schema

---
+++ Backend/src/models/feeStructure.model.js
@@ -0,0 +1,26 @@
+import mongoose,{Schema} from "mongoose";
+
+const feeStructureSchema = new Schema({
+  class: {
+    type: String,
+    required: true
+  },
+  fee: [{
+    feeType: {
+      type: String,
+      enum: ['Tuition Fee', 'Transport Fee', 'Other Fee'],
+      required: true
+    },
+    structure: [
+      {
+        title: { type: String, required: true },
+        dueDate: Date,
+        compulsory: { type: Boolean, default: true },
+        amount: { type: Number, required: true },
+        discount: { type: Number, default: 0 }
+      }
+    ]
+}]
+});
+
+export const FeeStructure = mongoose.model('FeeStructure', feeStructureSchema);
