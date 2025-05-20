548e8005ca6cbb85d4459f22bad54ae1df4e82ad
Mayank Sachin chougale <136550347+Mayank202004@users.noreply.github.com>
2025-05-20 17:12:42 +0530

    Bugs fixed for redundant login error
    Bugs fixed for redundant login error

--- Web Frontend/src/api/axiosInstance.js
+++ Web Frontend/src/api/axiosInstance.js
@@ -25,7 +25,8 @@ axiosInstance.interceptors.response.use(
   (response) => response, // If request is successful, return the response
   async (error) => {
     const message = error?.response?.data?.message || error.message || "Something went wrong";
-    toast.error(message); //  show toast for response errors
+    if(error?.response?.status != 401)
+      toast.error(message); //  show toast for response errors
     const originalRequest = error.config;
 
     // If the error is due to token expiry (401 Unauthorized), try refreshing the token
