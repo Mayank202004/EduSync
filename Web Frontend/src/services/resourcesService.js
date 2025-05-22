16df51089f3b4a0c210d769acf9bb15ed5d2ff7a
Mayank Sachin chougale <136550347+Mayank202004@users.noreply.github.com>
2025-05-22 12:13:49 +0530

    Added API call for fetching super admin resources
    Work on #50
    Added API call for fetching super admin resources

--- Web Frontend/src/services/resourcesService.js
+++ Web Frontend/src/services/resourcesService.js
@@ -3,11 +3,25 @@ import axiosInstance from '@/api/axiosInstance';
 const BASEURL = import.meta.env.VITE_API_BASE_URL;
 
 /**
- * 
+ * @desc Fetch resources for a student
  * @returns {Promise} - Promise resolving to the rsposn
  */
 export const getStudentsResources = async () =>{
     const response = await axiosInstance.get(`${BASEURL}/resource/me`);
+    return response.data;
+}
+
+// To Do
+export const getTeachersResources = async () =>{
+    return;
+}
+
+/**
+ * @desc Fetch all resources for admin
+ * @returns { Promise } - Promise resolving to the response
+ */
+export const getAdminResources = async () =>{
+    const response = await axiosInstance.get(`${BASEURL}/resource/classes`);
     return response.data;
 }
 
