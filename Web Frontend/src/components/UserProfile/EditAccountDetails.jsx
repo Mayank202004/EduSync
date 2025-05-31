1356d8a8af5adc415d705f44b7fed187c5a80aae
Akash Patil <hib6498@gmail.com>
2025-05-31 18:11:21 +0530

    refactor: Some initial refactoring for profile  - extracted input, button, titled_container as seperate components  - removed avatar update for the time being  - refactor in other profile components

--- Web Frontend/src/components/UserProfile/EditAccountDetails.jsx
+++ Web Frontend/src/components/UserProfile/EditAccountDetails.jsx
@@ -1,35 +1,94 @@
-const EditAccountDetails = ({ user }) => {
+import { useActionState } from "react";
+import toast from "react-hot-toast";
+import Input from "../ui/Input";
+import SimpleButton from "../ui/SimpleButton";
+import { updateUserApi } from "@/services/profileService";
+
+const FULLNAME_REGEX = /^[a-zA-Z\s]+$/;
+
+const accountInfoAction = async (prevState, formData) => {
+  const username = formData.get("username");
+  const fullName = formData.get("fullName");
+
+  if (!username?.trim() || !fullName?.trim()) {
+    toast.error("Username and Full Name cannot be empty");
+    return { username, fullName };
+  }
+
+  if (!FULLNAME_REGEX.test(fullName)) {
+    toast.error("Full Name should contain alphabets and spaces only");
+    return { username, fullName };
+  }
+
+  try {
+    const apiCall = () => updateUserApi(username, fullName);
+
+    const response = await toast.promise(apiCall(), {
+      loading: "Updating account details...",
+      success: "Profile updated successfully",
+      error: "Failed to update profile",
+    });
+
+    if (response.statusCode < 200 || response.statusCode > 299) {
+      toast.error(response.error || "Something went wrong while updating.");
+      return { username, fullName };
+    }
+
+    // Reload only on success
+    setTimeout(() => window.location.reload(), 200);
+    return { username, fullName };
+  } catch (err) {
+    const message =
+      err?.response?.data?.message ||
+      err?.message ||
+      "An unknown error occurred while updating your profile.";
+
+    toast.error(message);
+    return { username, fullName };
+  }
+};
+
+const EditAccountDetails = ({ accountInfo }) => {
+  const [accountData, formAction, isPending] = useActionState(
+    accountInfoAction,
+    {
+      username: accountInfo.username,
+      fullName: accountInfo.fullName,
+    }
+  );
+
   return (
-    <div className="w-[90%] sm:w-[85%] flex flex-col gap-5 border-1 mx-auto mb-10 py-6 px-6 sm:px-10 md:px-15 rounded-sm">
-      <label>
-        <span className="font-bold text-lg">Username</span>
-        <input
-          type="text"
-          name="username"
-          className="ring-1 p-2 w-full my-1.5 rounded-sm text-black dark:text-white"
-          placeholder="Username"
-          defaultValue={user?.username}
-          required
+    <form className="space-y-6" action={formAction}>
+      <div className="flex flex-col gap-4 border-1 mx-auto py-5 px-6 sm:px-10 md:px-15 rounded-sm">
+        <Input
+          titleText="Username"
+          inputProps={{
+            name: "username",
+            type: "text",
+            placeholder: "Username",
+            required: true,
+            defaultValue: accountData.username,
+          }}
         />
-      </label>
-      <label>
-        <span className="font-bold text-lg">Full Name</span>
-        <input
-          type="text"
-          name="fullName"
-          className="ring-1 p-2 w-full my-1.5 rounded-sm text-black dark:text-white"
-          placeholder="Full name"
-          defaultValue={user?.fullName}
-          required
+        <Input
+          titleText="Full Name"
+          inputProps={{
+            name: "fullName",
+            type: "text",
+            placeholder: "Full Name",
+            required: true,
+            defaultValue: accountData.fullName,
+          }}
+          hints={[
+            "Format: <first name> <middle name> <last name>",
+            "E.g: Mayank Sachin Chougale",
+          ]}
         />
-        <span className="text-gray-500 dark:text-gray-400">
-          Format: &lt;first name&gt; &lt;middle name&gt; &lt;last name&gt;
-        </span>
-        <span className="block leading-4 text-gray-500 dark:text-gray-400">
-          E.g: Mayank Sachin Chougale
-        </span>
-      </label>
-    </div>
+      </div>
+      <SimpleButton buttonProps={{ type: "submit", disabled: isPending }}>
+        {isPending ? "Saving..." : "Save"}
+      </SimpleButton>
+    </form>
   );
 };
 
--- Web Frontend/src/pages/UserProfile.jsx
+++ Web Frontend/src/pages/UserProfile.jsx
@@ -3,84 +3,81 @@ import AvatarIcon from "@/components/Topbar/AvatarIcon";
 import { capitalizeFirstLetter } from "@/lib/utils";
 import EditAccountDetails from "@/components/UserProfile/EditAccountDetails";
 import PhotoPreview from "@/components/UserProfile/PhotoPreview";
-import { useState, useActionState } from "react";
-import toast from "react-hot-toast";
-import { compressImage } from "@/lib/compressImage";
-import updateUserApi from "@/services/profileService";
-
-const FULLNAME_REGEX = /^[a-zA-Z\s]+$/
+import TitledContainer from "@/components/ui/TitledContainer";
 
 const UserProfile = () => {
   const { user } = useAuth();
-  const [blob, setBlob] = useState(null);
-  const [profileData, formAction, isPending] = useActionState(
-    async (prevState, formData) => {
-      const username = formData.get("username");
-      const fullName = formData.get("fullName");
+  // const [blob, setBlob] = useState(null);
+  // const [profileData, formAction, isPending] = useActionState(
+  //   async (prevState, formData) => {
+  //     const username = formData.get("username");
+  //     const fullName = formData.get("fullName");
 
-      if (!username?.trim() || !fullName?.trim()) {
-        toast.error("Username and Full Name cannot be empty");
-        return prevState;
-      }
-      
-      if (!(FULLNAME_REGEX.test(fullName))) {
-        toast.error("Username should have alphabets and spaces only");
-        return prevState;
-      }
+  //     if (!username?.trim() || !fullName?.trim()) {
+  //       toast.error("Username and Full Name cannot be empty");
+  //       return prevState;
+  //     }
 
-      try {
-        const tempFormData = new FormData();
-        if (blob) { //compress if new image was selected
-          const compressedBlob = await compressImage(blob)
-          tempFormData.append("avatar", compressedBlob);
-        }
+  //     if (!FULLNAME_REGEX.test(fullName)) {
+  //       toast.error("Username should have alphabets and spaces only");
+  //       return prevState;
+  //     }
 
-        const apiCall = async () => {
-          return await updateUserApi(
-            username,
-            fullName,
-            blob ? tempFormData : null
-          );
-        };
+  //     try {
+  //       const tempFormData = new FormData();
+  //       if (blob) {
+  //         //compress if new image was selected
+  //         const compressedBlob = await compressImage(blob);
+  //         tempFormData.append("avatar", compressedBlob);
+  //       }
 
-        const { userDataRes, avatarDataRes } = await toast.promise(apiCall, {
-          loading: "Updating profile...",
-          success: "Profile updated successfully",
-          error: "Failed to update profile",
-        });
+  //       const apiCall = async () => {
+  //         return await updateUserApi(
+  //           username,
+  //           fullName,
+  //           blob ? tempFormData : null
+  //         );
+  //       };
 
-        // If backend sends a specific error inside the response (e.g. validation fail)
-        if (userDataRes?.data?.error || avatarDataRes?.data?.error) {
-          toast.error(
-            userDataRes?.data?.error ||
-              avatarDataRes?.data?.error ||
-              "Something went wrong"
-          );
-          return {...prevState, username, fullName};
-        }
+  //       const { userDataRes, avatarDataRes } = await toast.promise(apiCall, {
+  //         loading: "Updating profile...",
+  //         success: "Profile updated successfully",
+  //         error: "Failed to update profile",
+  //       });
 
-        // All went fine — refresh page or update UI
-        setTimeout(() => window.location.reload(), 200);
-        return {username, fullName}
-      } catch (err) {
-        const message =
-          err.response?.data?.message ||
-          err.message ||
-          "Unknown error occurred";
-        toast.error(message);
-        return {...prevState, username, fullName};
-      }
-    },
-    {
-      username: user.username,
-      fullName: user.fullName,
-      avatar: user.avatar,
-    }
-  );
+  //       // If backend sends a specific error inside the response (e.g. validation fail)
+  //       if (userDataRes?.data?.error || avatarDataRes?.data?.error) {
+  //         toast.error(
+  //           userDataRes?.data?.error ||
+  //             avatarDataRes?.data?.error ||
+  //             "Something went wrong"
+  //         );
+  //         return { ...prevState, username, fullName };
+  //       }
 
-  const handleBlob = (blobData) => {
-    setBlob(blobData);
-  };
+  //       // All went fine — refresh page or update UI
+  //       setTimeout(() => window.location.reload(), 200);
+  //       return { username, fullName };
+  //     } catch (err) {
+  //       const message =
+  //         err.response?.data?.message ||
+  //         err.message ||
+  //         "Unknown error occurred";
+  //       toast.error(message);
+  //       return { ...prevState, username, fullName };
+  //     }
+  //   },
+  //   {
+  //     username: user.username,
+  //     fullName: user.fullName,
+  //     avatar: user.avatar,
+  //   }
+  // );
+
+  // const handleBlob = (blobData) => {
+  //   setBlob(blobData);
+  // };
+
 
   return (
     <div className="flex flex-col min-h-full w-full bg-customLightBg dark:bg-customDarkBg">
@@ -88,26 +85,14 @@ const UserProfile = () => {
       <h3 className="mx-auto text-lg text-gray-400">
         {capitalizeFirstLetter(user.role)}
       </h3>
-      <form
-        action={formAction}
-        className="min-w-2xs max-w-3xl w-full grid py-6 mx-auto bg-white dark:bg-customDarkFg rounded-md my-6"
-      >
-        <h2 className="font-bold mx-6 text-lg sm:mx-10 md:mx-15 mt-6 mb-2.5">
-          Photo preview
-        </h2>
-        <PhotoPreview user={profileData} onBlobReady={handleBlob} />
-        <h2 className="font-bold mx-6 text-lg sm:mx-10 md:mx-15 mt-4 mb-2.5">
-          Account details
-        </h2>
-        <EditAccountDetails user={profileData} />
-        <button
-          type="submit"
-          disabled={isPending}
-          className="cursor-pointer py-2 px-4 w-fit mr-10 ml-auto rounded-sm bg-blue-300 dark:bg-blue-400 hover:bg-blue-400 dark:hover:bg-blue-300 duration-200"
-        >
-          {isPending ? "Saving..." : "Save"}
-        </button>
-      </form>
+      <div className="min-w-2xs max-w-3xl w-full flex flex-col gap-6 items-center mx-auto rounded-md my-6">
+        {/* <TitledContainer title="Photo Preview" containerStyle="px-2 sm:px-6">
+          <PhotoPreview user={profileData} onBlobReady={handleBlob} />
+        </TitledContainer> */}
+        <TitledContainer title="Account details">
+          <EditAccountDetails accountInfo={user} />
+        </TitledContainer>
+      </div>
     </div>
   );
 };
