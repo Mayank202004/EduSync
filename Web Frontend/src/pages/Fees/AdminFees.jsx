f3f6a3eb89f704d60791c806f1cdec88a11f753c
Mayank Sachin chougale <136550347+Mayank202004@users.noreply.github.com>
2025-06-09 15:45:34 +0530

    Added RBAC setup for fees
    Work on #89
    Added RBAC setup for fees

---
+++ Web Frontend/src/pages/Fees/AdminFees.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AdminFees() {
+  return (
+    <div>AdminFees</div>
+  )
+}
+
+export default AdminFees
---
+++ Web Frontend/src/pages/Fees/index.jsx
@@ -0,0 +1,31 @@
+import { lazy, Suspense } from 'react';
+import { useAuth } from '@/auth/AuthContext';
+import LoadingScreen from '@/components/Loading';
+
+const StudentFees = lazy(() => import('./StudentFees'));
+const AdminFees = lazy(() => import('./AdminFees'));
+
+export default function Resources() {
+  const { user } = useAuth();
+
+  if (!user) return null;
+
+  let Component;
+  switch (user.role) {
+    case 'student':
+      Component = StudentFees;
+      break;
+    case 'super admin':
+      Component = AdminFees;
+      break;
+    default:
+      return <div>Unauthorized</div>; // To DO: Make a Unauthorised page 
+  }
+
+  return (
+    <Suspense fallback={<LoadingScreen/>}>
+      <Component />
+    </Suspense>
+  );
+}
+// This code is a React component that conditionally renders different resource components based on user roles.
