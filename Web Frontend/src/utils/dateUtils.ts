0df75ab4f3c0d7d0acfb3cf5dccad9e6bbd19a05
Mayank Sachin chougale <136550347+Mayank202004@users.noreply.github.com>
2025-06-11 10:29:05 +0530

    Update dateUtils.ts
    Added util to convert date string to YYYY-MM-DD

--- Web Frontend/src/utils/dateUtils.ts
+++ Web Frontend/src/utils/dateUtils.ts
@@ -1,3 +1,8 @@
+/**
+ * @desc Formats date in the format 10 May 2025
+ * @param dateString - Date String
+ * @returns {String} - Date string in format 10 May 2025
+ */
 export function formatDate(dateString: string): string {
   const date = new Date(dateString);
   return date.toLocaleDateString("en-IN", {
@@ -6,3 +11,17 @@ export function formatDate(dateString: string): string {
     day: "numeric",
   });
 }
+
+/**
+ * @desc Formats date in the format YYYY-MM-DD
+ * @param dateStr - Date String
+ * @returns {String} - In hte format YYYY-MM-DD
+ */
+export const formatToYYYYMM_D = (dateStr) => {
+  const date = new Date(dateStr);
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0"); // 2-digit month
+  const day = date.getDate(); // No pad
+  return `${year}-${month}-${day}`;
+};
+
