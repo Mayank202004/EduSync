325ef67871d4eaddc433fc2b96e02ec5faab8687
hib6498 <113171812+hib6498@users.noreply.github.com>
2025-06-13 10:10:15 +0530

    feat.: 404 not found page

---
+++ Web Frontend/src/pages/NotFound404.jsx
@@ -0,0 +1,21 @@
+import notFoundImage from "@/assets/404.png";
+import LinkButton from "@/components/ui/LinkButton";
+
+const NotFound404 = () => {
+  return (
+    <div className="h-full w-full flex flex-col items-center justify-center">
+      <h1 className="text-5xl font-bold">Error 404</h1>
+      <p className="text-gray-500 text-lg">Resource not found</p>
+      <img
+        src={notFoundImage}
+        className="max-w-full max-h-1/2 object-contain"
+        alt="Resource not found image"
+      />
+      <LinkButton to={-1} className="text-sm" navigateProps={{ replace: true }}>
+        Go back
+      </LinkButton>
+    </div>
+  );
+};
+
+export default NotFound404;
