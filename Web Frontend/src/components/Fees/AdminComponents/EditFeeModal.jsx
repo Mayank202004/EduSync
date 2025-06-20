8540020b036c6b980929007ce0e3f337ae332b28
hib6498 <113171812+hib6498@users.noreply.github.com>
2025-06-20 18:43:40 +0530

    style fix in add fee modal

--- Web Frontend/src/components/Fees/AdminComponents/EditFeeModal.jsx
+++ Web Frontend/src/components/Fees/AdminComponents/EditFeeModal.jsx
@@ -128,7 +128,7 @@ const EditFeeModal = ({
           <button
             type="button"
             onClick={onClose}
-            className="px-4 py-2 bg-gray-300 rounded hover:bg-gray-400"
+            className="px-4 py-2 bg-gray-300 rounded dark:bg-gray-600 hover:bg-gray-400"
           >
             Cancel
           </button>
