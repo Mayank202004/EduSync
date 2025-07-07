f56a6601ef74aabff1bcacbb7ba9448bf23a03b0
Mayank Sachin chougale <136550347+Mayank202004@users.noreply.github.com>
2025-07-07 17:46:52 +0530

    Added typing loader UI with animation
    Work on #115
    Added typing loader UI with animation

---
+++ Web Frontend/src/components/Chat/Typing.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+const Typing = () => {
+  return (
+    <div className="p-1 w-fit inline-flex gap-1.5">
+      <span className="h-2 w-2 bg-zinc-300 rounded-full animate-bounce1"></span>
+      <span className="h-2 w-2 bg-zinc-300 rounded-full animate-bounce2"></span>
+      <span className="h-2 w-2 bg-zinc-300 rounded-full animate-bounce3"></span>
+    </div>
+  );
+};
+
+export default Typing;
+
