3bdc17ae71606347309f0bdfd136d9d008d11b9d
Mayank Sachin chougale <136550347+Mayank202004@users.noreply.github.com>
2025-07-09 17:24:07 +0530

    Update dashboard.controller.js
    Fetch super admin chats and all users

--- Backend/src/controllers/dashboard.controller.js
+++ Backend/src/controllers/dashboard.controller.js
@@ -1,7 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 import { ApiError } from "../utils/apiError.js";
-import { getStudentChats } from "./chats.controller.js";
+import { getAllUsers, getStudentChats, getSuperAdminChats } from "./chats.controller.js";
 import { getTeacherChats } from "./chats.controller.js";
 
 
@@ -16,4 +16,10 @@ export const fetchTeacherDashboardData = asyncHandler(async (req, res) => {
     const teacher = req.teacher;
     const chatData = await getTeacherChats(teacher);
     return res.status(200).json(new ApiResponse(200, {chatData}, "Teacher Dashboard data fetched successfully"));
+})
+
+export const fetchSuperAdminDashboardData = asyncHandler(async (req, res) => {
+    const allUsers = await getAllUsers(req.user._id);
+    const chatData = await getSuperAdminChats(req.user._id);
+    return res.status(200).json(new ApiResponse(200, {chatData,allUsers}, "Super Admin Dashboard data fetched successfully"));
 })
