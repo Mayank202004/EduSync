24ed999c70dbaeaa312888cd277411bd2afbb996
Mayank Sachin chougale <136550347+Mayank202004@users.noreply.github.com>
2025-07-12 23:06:09 +0530

    Update setting.routes.js
    Bug fixed

--- Backend/src/routes/setting.routes.js
+++ Backend/src/routes/setting.routes.js
@@ -7,7 +7,7 @@ const router = Router();
 
 router.route("/academic-year").get(verifyJWT, SettingController.getAcademicYear);
 router.route("/academic-year").put(verifyJWT,verifySuperAdmin, SettingController.setAcademicYear);
-router.route("school-name").get(verifyJWT, SettingController.getSchoolName);
-router.route("school-name").put(verifyJWT,verifySuperAdmin, SettingController.setSchoolName);
+router.route("/school-name").get(verifyJWT, SettingController.getSchoolName);
+router.route("/school-name").put(verifyJWT,verifySuperAdmin, SettingController.setSchoolName);
 
 export default router;
