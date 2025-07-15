00144b680d60759a8393e0d708706932d7039e72
hib6498 <113171812+hib6498@users.noreply.github.com>
2025-07-15 13:04:12 +0530

    feat: Completed image preview component  - integrated the component with resources preview
    Progress of #151

--- Web Frontend/src/components/Chat/ImagePreview.jsx
+++ Web Frontend/src/components/Chat/ImagePreview.jsx
@@ -1,13 +1,37 @@
 import useClickOutside from "@/hooks/useClickOutside";
+import { faClose } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import IconTextButton from "./IconTextButton";
 
 import ModalWrapper from "./ModalWrapper";
 
-const ImagePreview = ({ onClose, url }) => {
+const ImagePreview = ({ onClose, url, header = null }) => {
   const [containerRef] = useClickOutside(onClose);
   return (
     <ModalWrapper>
-      <div ref={containerRef} className="bg-customLightBg dark:bg-customDarkFg p-3">
-        <img className="max-h-[70vh] object-cover" src={url} alt="image" />
+      <div
+        ref={containerRef}
+        className="bg-customLightBg dark:bg-customDarkFg p-4 rounded-lg "
+      >
+        <div className="flex flex-nowrap gap-2 w-full items-center">
+          {header}
+          <IconTextButton
+            className="rounded-full overflow-hidden size-8 flex items-center justify-center ml-auto"
+            buttonProps={{ onClick: onClose }}
+            icon={
+              <FontAwesomeIcon
+                icon={faClose}
+                className="fa-lg text-gray-500"
+              ></FontAwesomeIcon>
+            }
+          />
+        </div>
+        <hr className="my-2" />
+        <img
+          className="max-h-[70vh] object-cover mx-auto"
+          src={url}
+          alt="image"
+        />
       </div>
     </ModalWrapper>
   );
