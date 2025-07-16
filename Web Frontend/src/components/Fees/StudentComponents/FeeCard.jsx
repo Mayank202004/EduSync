ca33ec941e224cd5541957c0ce866684a2558f1c
Mayank Sachin chougale <136550347+Mayank202004@users.noreply.github.com>
2025-07-16 21:51:05 +0530

    Redesign FeeCard layout and improve due date logic
    Refactored the FeeCard component to update its layout, moving the selection button and fee details into a more structured format. Replaced formatDateTime with formatDate, added a due soon indicator based on the due date, and improved styling for better clarity and usability.

--- Web Frontend/src/components/Fees/StudentComponents/FeeCard.jsx
+++ Web Frontend/src/components/Fees/StudentComponents/FeeCard.jsx
@@ -1,53 +1,67 @@
 import { cn } from "@/lib/cn";
-import { formatDateTime } from "@/utils/dateUtils";
+import { formatDate } from "@/utils/dateUtils";
 
 const FeeCard = ({
   feeData,
   selectedFees = null,
   selectFee = null,
   deselectFee = null,
-  isSelectable = true, //represents fees paid
+  isSelectable = true,
 }) => {
   const isSelected = selectedFees?.includes(feeData._id);
 
+  const today = new Date();
+  const dueDate = new Date(feeData.dueDate);
+  const thresholdDate = new Date(dueDate);
+  thresholdDate.setDate(thresholdDate.getDate() - 15);
+
+  const isDueSoon = today > thresholdDate;
+
   return (
     <li className="w-full shrink-0 flex md:px-2 pb-6">
-      {isSelectable && (
-        <button
-          type="button"
-          className={cn(
-            "cursor-pointer fade-in-translate-full h-fit relative mr-2 p-2 my-auto rounded-full border-black dark:border-white border-1 after:content-[''] after:inset-0 after:scale-[70%] after:rounded-full after:bg-blue-600 after:absolute after:hidden",
-            {
-              "after:block": isSelected,
-            }
-          )}
-          onClick={
-            isSelected
-              ? () => deselectFee(feeData._id)
-              : () => selectFee(feeData._id)
-          }
-        ></button>
-      )}
       <div
         className={cn(
-          "w-[95%] mx-auto p-3 border-1 bg-white dark:bg-customDarkFg border-gray-600 rounded-lg dark:border-gray-200",
+          "w-[95%] mx-auto p-4 border-1 bg-white dark:bg-customDarkFg rounded-md",
           {
             "border-2 border-blue-500 ring-blue-200 ring-2 shadow-md":
               isSelected,
           }
         )}
       >
-        <div className="flex justify-between">
-          <h2 className="font-bold text-2xl truncate w-fit">{feeData.title}</h2>
-          <h1 className="font-bold text-xl mr-2">&#x20B9;{feeData.amount}</h1>
+        <div className="flex items-center mb-2">
+          {isSelectable && (
+            <button
+              type="button"
+              className={cn(
+                "cursor-pointer fade-in-translate-full h-fit relative mr-2 p-2 my-auto rounded-full border-black dark:border-white border-1 after:content-[''] after:inset-0 after:scale-[70%] after:rounded-full after:bg-blue-600 after:absolute after:hidden",
+                {
+                  "after:block": isSelected,
+                }
+              )}
+              onClick={
+                isSelected
+                  ? () => deselectFee(feeData._id)
+                  : () => selectFee(feeData._id)
+              }
+            ></button>
+          )}
+          <h2 className="font-bold text-lg truncate">Pay {feeData.title}</h2>
         </div>
-        <p className="text-gray-600 dark:text-gray-400">
-          {isSelectable
-            ? `Due Date: ${formatDateTime(feeData.dueDate)}`
-            : `Paid On: ${formatDateTime(feeData.paidOn)}`}
-        </p>
-        <hr className="my-2" />
-        <p>Description: {feeData.title}</p>
+
+        <hr className="my-3 border-t-1 border-gray-300 dark:border-gray-600" />
+
+        <div className="flex justify-between items-center w-full px-10">
+          <h3 className="font-medium">{feeData.title}</h3>
+          <h3 className="font-medium">{formatDate(feeData.dueDate)}</h3>
+          {isDueSoon && (
+            <span className="text-sm text-red-600 bg-red-100 px-3 py-1 rounded-full font-semibold">
+              Due
+            </span>
+          )}
+          <h1 className="font-bold text-xl text-gray-800 dark:text-white">
+            ₹{feeData.amount.toLocaleString("en-IN")}
+          </h1>
+        </div>
       </div>
     </li>
   );
