feddcae505a0a409e11dea22fc94fdb050fa28c2
Mayank Sachin chougale <136550347+Mayank202004@users.noreply.github.com>
2025-08-07 23:52:02 +0530

    Added send OTP helper function
    Work on #217
    Added send OTP helper function
      - The send email login needs to be added later
     - Currently otp is console logged for testing purposes need to be removed later

---
+++ Backend/src/utils/otpUtil.js
@@ -0,0 +1,17 @@
+import crypto from "crypto";
+import { Otp } from "../models/otp.model.js";
+
+
+export const sendOtpHelper = async (email, purpose = "LOGIN") => {
+  const otp = crypto.randomInt(100000, 999999).toString();
+  const expiresAt = new Date(Date.now() + 5 * 60 * 1000); // 5 minutes
+
+  await Otp.create({ email, otp, purpose, expiresAt });
+
+//   await sendMail({
+//     to: email,
+//     subject: `Your OTP for ${purpose}`,
+//     text: `Your OTP is ${otp}. It will expire in 5 minutes.`,
+//   });
+console.log("otp : ",otp); // To Do : remove this and send mail to user
+};
