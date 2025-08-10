0d379a749f5996966fa534df316697f113f415e8
Mayank Sachin chougale <136550347+Mayank202004@users.noreply.github.com>
2025-08-10 23:07:31 +0530

    Added utils to generate random passwords and unique usernames
    Work on #216
    Added utils to generate random passwords and unique usernames

---
+++ Backend/src/utils/userUtils.js
@@ -0,0 +1,24 @@
+/**
+ * @desc  Helper to generate unique username
+ * @param {String} fullName - Full name of the user
+ * @returns {String} - Unique username
+ */
+export const generateUniqueUsername = async (fullName) => {
+    let baseUsername = fullName.split(" ")[0].toLowerCase();
+    let username = baseUsername;
+    let counter = 1;
+
+    while (await User.findOne({ username })) {
+        username = `${baseUsername}${counter}`;
+        counter++;
+    }
+    return username;
+};
+
+/**
+ * @desc Helper to generate random password
+ * @returns {String} - Random password
+ */
+export const generateRandomPassword = () => {
+    return crypto.randomBytes(6).toString("base64").replace(/[^a-zA-Z0-9]/g, "").slice(0, 8);
+};
