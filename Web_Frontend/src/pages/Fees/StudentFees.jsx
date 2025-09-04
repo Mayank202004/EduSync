eb5ec1e680c2beb9f7fcab812c45436a278ae50c
Mayank Sachin chougale <136550347+Mayank202004@users.noreply.github.com>
2025-09-04 09:56:56 +0530

    Student Fees section height issue fixed
    Student Fees section height issue fixed

--- Web_Frontend/src/pages/Fees/StudentFees.jsx
+++ Web_Frontend/src/pages/Fees/StudentFees.jsx
@@ -34,7 +34,7 @@ const Fees = () => {
   }
 
   return (
-    <div className="tablet:flex min-h-screen w-full px-4  bg-customLightBg dark:bg-zinc-950">
+    <div className="tablet:flex h-full w-full px-4  bg-customLightBg dark:bg-zinc-950">
       {/* Left Sidebar */}
       <div className="tablet:sticky self-start top-0 left-0 bottom-0 h-fit tablet:w-3/10 pr-3">
         <div className="bg-white dark:bg-zinc-900 shadow-md rounded-sm flex flex-col items-start gap-3 px-3 py-2 mt-5">
@@ -55,7 +55,7 @@ const Fees = () => {
           <FeeCardSkeleton />
         </div>
       ) : (
-        <div className="tablet:min-w-[65%] p-4 tablet:mr-0 w-full tablet:h-full border-x">
+        <div className="tablet:min-w-[65%] p-4 tablet:mr-0 w-full tablet:h-full border-x overflow-auto pb-10">
           <PendingFees isPending={isPending} feesData={feesData.pending} />
           <hr className="mt-6" />
           <PaidFees isPaid={isPaid} feesData={feesData.paid} />
