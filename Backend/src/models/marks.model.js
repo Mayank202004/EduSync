ac58ea599f555e2dc2fba6c898918289010931c8
Mayank Sachin chougale <136550347+Mayank202004@users.noreply.github.com>
2025-09-13 10:55:44 +0530

    Added model for student marks
    Work on #284
    Added model for student marks

---
+++ Backend/src/models/marks.model.js
@@ -0,0 +1,23 @@
+// models/ClassMarks.js
+import mongoose from "mongoose";
+
+const studentMarkSchema = new mongoose.Schema({
+  studentId: { type: mongoose.Schema.Types.ObjectId, ref: "Student", required: true },
+  marksObtained: { type: Number, required: true },
+  totalMarks: { type: Number, required: true },
+});
+
+const subjectMarksSchema = new mongoose.Schema({
+  subject: { type: String, required: true },
+  students: [studentMarkSchema],
+  gradedBy: { type: mongoose.Schema.Types.ObjectId, ref: "Teacher" },
+});
+
+const classMarksSchema = new mongoose.Schema({
+  examId: { type: mongoose.Schema.Types.ObjectId, ref: "Exam", required: true },
+  class: { type: String, required: true },
+  div: { type: String, required: true },
+  subjects: [subjectMarksSchema],
+}, { timestamps: true });
+
+export default mongoose.model("ClassMarks", classMarksSchema);
---
+++ Backend/src/models/studentMarks.model.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+/**
+ * @desc This model is created (which overlaps marks.model) to reduce the complexity of fetching for a single student
+*/
+const studentMarksSchema = new mongoose.Schema({
+  examId: { type: mongoose.Schema.Types.ObjectId, ref: "Exam", required: true },
+  studentId: { type: mongoose.Schema.Types.ObjectId, ref: "Student", required: true },
+  marks: [
+    {
+      subject: { type: String, required: true },
+      marksObtained: { type: Number, required: true },
+      totalMarks: { type: Number, required: true },
+      markedBy: { type: mongoose.Schema.Types.ObjectId, ref: "Teacher" },
+    }
+  ]
+}, { timestamps: true });
+
+export default mongoose.model("StudentMarks", studentMarksSchema);
